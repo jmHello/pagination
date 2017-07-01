c975768f82834e8d42319e6512b93bd2c286e81a
jm <471938302@qq.com>
2017-07-01 11:41:04 +0800

    20170701 pagination finish

--- library/jmModule/j_plugIn.js
+++ library/jmModule/j_plugIn.js
@@ -6,27 +6,30 @@
 * */
 (function ($) {
     $.CustormEvent=function () {
-        this.clientList={};
+        this.clientList={};//缓存列表
     };
     var fn=$.CustormEvent.prototype;
+    //发布消息
     fn.trigger=function () {
-        var key=Array.prototype.shift.call(arguments),
-            fns=this.clientList[key];
-        if(!fns||fns.length==0) return false;
-        for(var i=0,fn;fn=fns[i++];) fn.apply(this,arguments);
+        var key=Array.prototype.shift.call(arguments),//取出消息类型
+            fns=this.clientList[key];// 取出该消息对应的回调函数集合
+        if(!fns||fns.length==0) return false;//如果没有订阅该消息，则返回
+        for(var i=0,fn;fn=fns[i++];) fn.apply(this,arguments); //执行缓存列表中的函数（ arguments 是发布消息时附送的参数）
     };
+    //订阅消息
     fn.listen=function (key,fn) {
-        if(!this.clientList[key])this.clientList[key]=[];
-        this.clientList[key].push(fn);
+        if(!this.clientList[key])this.clientList[key]=[];//如果还没有订阅过此类消息，给该类消息创建一个缓存列表
+        this.clientList[key].push(fn);// 订阅的消息添加进消息缓存列表
     };
+    //删除消息
     fn.remove=function (key,fn) {
         var fns=this.clientList[key];
-        if(!fns) return false;
-        if(!fn) fn&&(fns.length=0);
+        if(!fns) return false; // 如果 key 对应的消息没有被人订阅，则直接返回
+        if(!fn) fn&&(fns.length=0); // 如果没有传入具体的回调函数，表示需要取消 key 对应消息的所有订阅
         else{
-            for(var l=fns.length,_fn;l--;){
+            for(var l=fns.length,_fn;l--;){ // 反向遍历订阅的回调函数列表
                 _fn=fns[l];
-                if(_fn===fn) fns.splice(l,1);
+                if(_fn===fn) fns.splice(l,1); // 删除订阅者的回调函数
             }
         }
     }
